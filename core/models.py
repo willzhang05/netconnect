e6e3ed773ea2d5d9d2ad12102a337b7e9607e910
Connor Moon <cdm3mu@virginia.edu>
2021-03-26 18:24:42 -0400

    simple models

--- core/models.py
+++ core/models.py
@@ -10,6 +10,16 @@ class Profile(models.Model):
     gender = models.CharField(max_length=50)
     ethnicity = models.CharField(max_length=50)
     bio = models.TextField(max_length=300)
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.question_text
+ 
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.choice_text
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
