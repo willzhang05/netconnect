4f839dd20b04ff442405816dd74dad7fa8e827d2
WilliamAustinMcCarty <78712326+WilliamAustinMcCarty@users.noreply.github.com>
2021-03-26 06:05:42 -0400

    added 2 new views
    Added a definition for the questionnaire view. It uses a questionnaire.html template.
    Added a definition for the profile view. It uses a questionnaire.html profile.

--- core/views.py
+++ core/views.py
@@ -5,6 +5,13 @@ from django.contrib.auth.decorators import login_required
 def login(request):
   return render(request, 'login.html')
 
+def questionnaire(request):
+  return render(request, 'questionnaire.html')
+
+def profile(request):
+  return render(request, 'profile.html')
+
+
 #@login_required
 #def home(request):
 #  return render(request, 'index.html')
