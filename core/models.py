1b80ba894c80907187143e5cdc51591d4e3e4567
Connor Moon <cdm3mu@virginia.edu>
2021-03-28 11:09:53 -0400

    made intfield

--- core/models.py
+++ core/models.py
@@ -17,7 +17,7 @@ class Question(models.Model):
  
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.IntegerField(default = 0)
     def __str__(self):
         return self.choice_text
 
