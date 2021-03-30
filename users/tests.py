11d7ce2b74deec6a46b539a0f20118712710051e
WilliamAustinMcCarty <78712326+WilliamAustinMcCarty@users.noreply.github.com>
2021-03-30 11:11:40 -0400

    first 2 user tests
    first 2 user tests. 
    First, test default value of politics. 
    Then, test if changed politics values are maintained.

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from .models import CustomUser
 # Create your tests here.
+
+
+class CustomUserTestCase(TestCase):
+
+    def defaultValues(self):
+        user1 = CustomUser()
+        assert user1.politics == "U"
+
+    def changeValue(self):
+        user2 = CustomUser()
+        user2.politics = "C"
+        assert user2.politics == "C"
+
