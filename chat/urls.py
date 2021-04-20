2b6130859bf1f621ce70e4b1ae5e009e9d0a550a
WilliamAustinMcCarty <78712326+WilliamAustinMcCarty@users.noreply.github.com>
2021-04-20 07:29:15 -0400

    fixed a problem where profile got changed
    fixed a problem where profile got changed. The buttons currently send you to the chat, but we want it to send the user to chat/username. This is a simple fix I am leaving to connor

--- chat/urls.py
+++ chat/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('chat/', views.index, name='index'),
-    path('chat/<str:room_name>/', views.room, name='room'),
+    path('chat/', views.index, name='chat_index'),
+    path('chat/<str:room_name>/', views.room, name='chat_room'),
 ]
